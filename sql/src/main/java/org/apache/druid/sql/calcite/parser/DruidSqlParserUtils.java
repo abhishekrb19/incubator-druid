508f705b6a551f92c2d10cff7a43a44b2da7a2b0
Abhishek Balaji Radhakrishnan <abhishek.rb19@gmail.com>
2024-12-20 16:52:51 -0800

    Revert "Remove support for CURRENT_TIMESTAMP as it will require accounting for granularity as well."
    This reverts commit 5e61c286dbdfe9bdff68bbbaa9f043753e039c9f. Going to try plumbing in granularity.

--- sql/src/main/java/org/apache/druid/sql/calcite/parser/DruidSqlParserUtils.java
+++ sql/src/main/java/org/apache/druid/sql/calcite/parser/DruidSqlParserUtils.java
@@ -646,9 +646,14 @@ public class DruidSqlParserUtils
       return String.valueOf(zonedTimestamp.toInstant().toEpochMilli());
     }
 
-    if (sqlNode instanceof SqlIdentifier && ("CURRENT_DATE".equalsIgnoreCase(((SqlIdentifier) sqlNode).getSimple()))) {
-      final DateTime currentDateTime = LocalDate.now().toDateTimeAtStartOfDay();
-      return String.valueOf(currentDateTime.getMillis());
+    if (sqlNode instanceof SqlIdentifier && ("CURRENT_DATE".equalsIgnoreCase(((SqlIdentifier) sqlNode).getSimple()) || "CURRENT_TIMESTAMP".equalsIgnoreCase(((SqlIdentifier) sqlNode).getSimple()))) {
+      final DateTime currentDateTime;
+      if ("CURRENT_DATE".equalsIgnoreCase(((SqlIdentifier) sqlNode).getSimple())) {
+        currentDateTime = LocalDate.now().toDateTimeAtStartOfDay();
+      } else {
+        currentDateTime = LocalDate.now().toDateTimeAtCurrentTime();
+      }
+      return String.valueOf(currentDateTime.toDateTimeAtStartOfDay().getMillis());
     }
 
     if (sqlNode instanceof SqlBasicCall) {
@@ -660,8 +665,14 @@ public class DruidSqlParserUtils
         SqlNode rightOperand = basicCall.getOperandList().get(1);
 
           if (leftOperand instanceof SqlIdentifier
-            && ("CURRENT_DATE".equalsIgnoreCase(((SqlIdentifier) leftOperand).getSimple()))) {
-            final DateTime currentDateTime = LocalDate.now().toDateTimeAtStartOfDay();
+            && ("CURRENT_DATE".equalsIgnoreCase(((SqlIdentifier) leftOperand).getSimple()) || "CURRENT_TIMESTAMP".equalsIgnoreCase(((SqlIdentifier) leftOperand).getSimple()))) {
+
+            final DateTime currentDateTime;
+            if ("CURRENT_DATE".equalsIgnoreCase(((SqlIdentifier) leftOperand).getSimple())) {
+              currentDateTime = LocalDate.now().toDateTimeAtStartOfDay();
+            } else {
+              currentDateTime = LocalDate.now().toDateTimeAtCurrentTime();
+            }
 
           Duration duration = extractInterval(rightOperand);
 
